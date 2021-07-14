18ca664d7752bedcb5ee61dc131d60fdcbc5f62e
Dimas Purwanto <dimasmaspur@gmail.com>
2021-07-14 18:29:27 +0700

    botak

---
+++ application/views/pesanan.php
@@ -0,0 +1,68 @@
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800">Pesanan Saya</h1>
+<!-- DataTales Example -->
+<div class="card shadow mb-4 mt-2">
+    <div class="card-header py-3">
+        <span class="badge badge-info">Pesanan kamu sedang kami persiapan</span>
+    </div>
+    <div class="card-body">
+
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No Pesanan</th>
+                        <th>Tanggal</th>
+                        <th>Nama Menu</th>
+                        <th>Qty</th>
+                        <th>Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                <?php 
+                    $no = 1;
+                    foreach($datamenu as $data => $menu){
+                ?>
+                    <tr>
+                        <td><?= $menu['id'];?></td>
+                        <td><?= $menu['tanggal'];?></td>
+                        <td><?= $menu['nama'];?></td>
+                        <td><?= $menu['qty'];?></td>
+                        <td>Rp. <?= $menu['subtotal'];?></td>
+                    </tr>
+                <?php
+                    
+                $no++;
+                }
+                ?>
+
+                
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+</div>
+<div class="row">
+<div class="col-xl-10 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Terima Kasih Telah Memesan di Rumah Makan Haji Muhammad</div>
+                                            <div class="h5 mb-0 mt-4 font-weight-bold text-gray-800">Total : Rp. <?=$datatotal;?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+</div>
+</div>
+
